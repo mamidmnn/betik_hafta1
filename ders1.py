c3c1342c8c6580007f1310e763f6bec2a75d7c01
mamidmnn <mahmutduman913@gmail.com>
2025-06-15 17:41:09 +0300

    ders1.py

--- ders1.py
+++ ders1.py
@@ -1,6 +1,6 @@
 ﻿
 
-# 1. İsim listesi işleme
+
 isimler = ['merve acar', 'yaren parlak', 'ali efe akar']
 for sahis in isimler:
     isimlistesi = sahis.split(' ')
@@ -9,7 +9,7 @@ for sahis in isimler:
         print(isim)
     print('======= Üst döngü bitiyor =======')
 
-# 2. Her ismin kaç harfli olduğunu yazdır
+
 isimler = ['merve acar', 'yaren parlak', 'efe akar']
 nl = []
 for sahis in isimler:
@@ -19,13 +19,13 @@ for sahis in isimler:
         print(f"{kelime} kelimesinin içinde {len(kelime)} harf var")
 print(nl)
 
-# 3. Kelimelerin son 3 harfini ve harf sayısını yazdır
+
 isim = 'merve acar'
 for kelime in isim.split():
     print(kelime[-3:])
     print(len(kelime))
 
-# 4. Listedeki son 3 elemanı yazdır
+
 lls = [2, 3, 4, 2, 3, 6, 5, 8, 9, 11, 10, 20, 12]
 for i in lls[-3:]:
     print(i)
